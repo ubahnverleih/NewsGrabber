c8e8759594680394530d22b490ae39b92cf445a2
Constantin Müller <ubahnverleih@gmail.com>
2018-01-16 22:44:02 +0100

    updated web__heute_de

--- services/web__heute_de.py
+++ services/web__heute_de.py
@@ -1,9 +1,9 @@
 refresh = 300
-version = 20161106.01
+version = 20180116.01
 
-urls = ['http://www.heute.de/']
-regex = [r'https?:\/\/[^\/]*heute\.de']
+urls = ['http://www.heute.de/','https://www.zdf.de/nachrichten/']
+regex = [r'https?:\/\/[^\/]*heute\.de',r'https?:\/\/[^\/]*zdf\.de']
 videoregex = []
 liveregex = []
-
+wikidata = 'Q48989'
 
