e99ea5f3f8ae8df2978baa2f27fae29838b85977
Constantin Müller <ubahnverleih@gmail.com>
2018-01-16 23:23:50 +0100

    updated web__dw_com added all languages
    (not all languages have full rss feeds)

--- services/web__dw_com.py
+++ services/web__dw_com.py
@@ -1,12 +1,41 @@
 refresh = 7200
-version = 20161106.01
+version = 20180116.01
 
 urls = ['http://www.dw.com/en/top-stories/s-9097',
+	'http://www.dw.com/de/themen/s-9077',
 	'http://rss.dw.com/rdf/rss-en-all',
 	'http://rss.dw.com/rdf/rss-de-all',
+	'http://rss.dw.com/rdf/rss-alb-all',
+	'http://www.dw.com/am/%E1%8B%AD%E1%8B%98%E1%89%B5/s-11646',
+	'http://rss.dw.de/rdf/rss-ar-all',
+	'http://www.dw.com/bn/%E0%A6%AC%E0%A6%BF%E0%A6%B7%E0%A7%9F/s-11929',
+	'http://rss.dw-world.de/rdf/rss-bos-all',
+	'http://www.dw.com/bg/%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D0%BE/s-10257',
+	'http://partner.dw.com/rdf/rss-chi-all',
+	'http://partner.dw.com/rdf/rss-cro-all',
+	'http://rss.dw.com/rdf/rss-dar-all',
+	'http://rss.dw.com/rdf/rss-fre-all',
+	'http://www.dw.com/el/%CE%B8%CE%B5%CE%BC%CE%B1%CF%84%CE%B1/s-10507',
+	'http://www.dw.com/ha/batutuwa/s-11603',
+	'http://www.dw.com/hi/%E0%A4%96%E0%A4%AC%E0%A4%B0%E0%A5%87%E0%A4%82/s-11931',
+	'http://rss.dw.com/rdf/rss-ind-all',
+	'http://www.dw.com/sw/idhaa-ya-kiswahili/s-11588',
+	'http://www.dw.com/mk/%D1%82%D0%B5%D0%BC%D0%B8/s-10339',
+	'http://www.dw.com/ps/%D8%AF%D9%88%D9%8A%DA%86%D9%87-%D9%88%D9%8A%D9%84%D9%87-%D9%BE%DA%9A%D8%AA%D9%88/s-11722',
+	'http://rss.dw.com/rdf/rss-per-all',
+	'http://partner.dw.com/xml/rss-pol-all',
+	'http://www.dw.com/pt-002/not%C3%ADcias/s-13918',
+	'http://www.dw.com/pt-br/not%C3%ADcias/s-7111',
+	'http://www.dw.com/ro/focus/s-10575',
+	'http://rss.dw.de/xml/rss-ru-all',
+	'http://rss.dw.com/rdf/serbian_all',
+	'http://www.dw.com/es/actualidad/s-30684',
+	'http://partner.dw.com/rdf/rss-tur-all',
+	'http://rss.dw.com/rdf/rss-ukr-all',
+	'http://www.dw.com/ur/%D8%B9%D9%86%D9%88%D8%A7%D9%86%D8%A7%D8%AA/s-11933',
 	'http://www.dw.com/en/media-center/all-media-content/s-100826']
 regex = [r'^https?:\/\/[^\/]*dw\.com\/']	
 videoregex = [r'\/av-']
 liveregex = []
-
+wikidata = 'Q153770'
 
