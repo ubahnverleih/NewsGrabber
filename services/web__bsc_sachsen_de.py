ac278fa2ae86a11ab3e10e592313f337978a4982
Constantin Müller <ubahnverleih@gmail.com>
2018-01-17 19:52:29 +0100

    added german local BSC Sachsen News network

---
+++ services/web__bsc_sachsen_de.py
@@ -0,0 +1,14 @@
+refresh = 1800
+version = 20180116.01
+
+urls = ['https://www.radiodresden.de/nachrichten/dresden/',
+	'https://www.radioleipzig.de/nachrichten/leipzig/',
+	'https://www.radiochemnitz.de/nachrichten/chemnitz/',
+	'https://www.radiolausitz.de/nachrichten/lausitz/',
+	'https://www.radiozwickau.de/nachrichten/zwickau/',
+	'https://www.radioerzgebirge.de/nachrichten/erzgebirge/']
+regex = [r'https?:\/\/[^\/]*radiodresden\.de', r'https?:\/\/[^\/]*radioleipzig\.de', r'https?:\/\/[^\/]*radiochemnitz\.de', r'https?:\/\/[^\/]*radiolausitz\.de', r'https?:\/\/[^\/]*radiozwickau\.de', r'https?:\/\/[^\/]*radioerzgebirge\.de']
+videoregex = []
+liveregex = []
+wikidata = 'Q2210561'
+
