8b71961c08e311e782501e831b725e10c7989f53
Constantin Müller <ubahnverleih@gmail.com>
2018-01-17 01:00:27 +0100

    added heise_de and golem_de from issue #6

---
+++ services/web__golem_de.py
@@ -0,0 +1,9 @@
+refresh = 1800
+version = 20180116.01
+
+urls = ['https://www.golem.de/','https://rss.golem.de/rss.php?feed=RSS0.91']
+regex = [r'https?:\/\/[^\/]*golem\.de']
+videoregex = [r'\/audio-video\/']
+liveregex = []
+wikidata = 'Q1536243'
+
---
+++ services/web__heise_de.py
@@ -0,0 +1,9 @@
+refresh = 1800
+version = 20180116.01
+
+urls = ['https://www.heise.de/','https://www.heise.de/newsticker/heise-atom.xml']
+regex = [r'https?:\/\/[^\/]*heise\.de']
+videoregex = [r'\/video\/']
+liveregex = []
+wikidata = 'Q1601295'
+
